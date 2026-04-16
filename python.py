1c13ba6d21de267877bf7a1e4a8725ac55ed0b61
saravanan <saravanan.mani3030@gmail.com>
2026-04-16 22:17:06 +0530

    mixedlist.insert('nive',3) python till 16/4/26

--- python.py
+++ python.py
@@ -7,17 +7,45 @@ if __name__ == "__main__":
 
 sample_fn() """
 
+print(8**10)
+"""
+// - to get qoutient
+/ - to get float value
+% - to get reminder
+
+"""
 print('hello world')
 print(5+7)
+2 + 7 * 8 # no error will occur as python works as an scripting language with shell.
 print(4//3)
 nums =[1,2,3]
 text = ['sara','nive','sarvedhitha']
+# LIST : which is collection of values and it can hold values of diff types
+# also we can have list of lists like mix = [nums,text] in which nums = 0 and text = 1
+# to get into either nums or text we need [x][y] matrix of indexing to get the element
+# has in-built fn's like .remove - removes by value, .pop - removes by index {LIFO}
+# .insert(u,v) - inserts something after u = index : v = value.
 mixedlist = nums + text
 print(mixedlist.append(45))
 print(mixedlist)
-print('sara\'s')
+print('sara\'s') 
+# to nullify special literls like "" ,'' , \ , \n ...etc 
+# In print statement we use \ backslash to nullify syntax error
 note = """
 Hi everyone 
 Good morning
 """
-print(note)
+print(note[2:])
+print(note[3:7])
+#slicing i.e: [x:y] where x is included and y is excluded
+# [x:] goes till end from x index
+# [:y] starts from 0 index and goes till y-1 index
+# we can also minus indexing in python which is helpfull for example if you want to print 
+# let's say last 'n' in saravanan we can use -1 
+# as indexing goes for saravanan in minus order like -1 to -9
+name = 'saravanan'
+print(name[-1]) 
+print(len(name))
+example_for_del = [3,4,5,6,7,8]
+del(example_for_del[3:])
+print(example_for_del)
