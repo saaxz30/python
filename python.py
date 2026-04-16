5037ef6009a38ac1150290486293554c82dd8efb
saravanan <saravanan.mani3030@gmail.com>
2026-04-16 21:23:24 +0530

    python-basics

---
+++ python.py
@@ -0,0 +1,23 @@
+""" def sample_fn():
+    if 5>2:
+        print("5 is greater than 2")
+
+if __name__ == "__main__":
+    sample_fn()
+
+sample_fn() """
+
+print('hello world')
+print(5+7)
+print(4//3)
+nums =[1,2,3]
+text = ['sara','nive','sarvedhitha']
+mixedlist = nums + text
+print(mixedlist.append(45))
+print(mixedlist)
+print('sara\'s')
+note = """
+Hi everyone 
+Good morning
+"""
+print(note)
